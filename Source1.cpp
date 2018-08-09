a799663236231307971019aa4b68ff25c95711a2
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-08-09 11:36:58 +0430

    Add files via upload

--- Source1.cpp
+++ Source1.cpp
@@ -1,32 +1,20 @@
 #include <iostream>
-
 using namespace std;
 
-int maximum(int, int, int);		//function prototype
+void main()
+{
+	const int arraySize = 10;
+	int total = 0, i;
+	int a[arraySize];
+	for (i = 0; i < arraySize; i++)
+	{
+		cout << "Enter number " << i << " : ";
+		cin >> a[i];
+	}
+	for (i = 0; i < arraySize; i++)
+		total += a[i];
+	cout << "Total of array element values is " << total << endl;
+	system("pause");
 
-int main() {
-	int a;
-	int b;
-	int c;
 
-	cout << "enter 3 integers";
-	cin >> a >> b >> c;
-	 
-	cout << "maximum is" << maximum(a, b, c) << \n;
-
-	return 0;
-
-}
-
-int maximum(int x, int y, int z) {
-
-	int max = x;
-
-	if (y > max)
-		max = y
-
-		if (z > max)
-			max = z;
-
-	return max;
 }
