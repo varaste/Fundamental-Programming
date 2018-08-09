c2a29e77fbb0e327cfa0fec9125714867ee7ba72
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-08-09 11:37:08 +0430

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,23 +1,14 @@
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
-#include <iomanip>
-
 using namespace std;
-
 void main()
 {
-	int a[10][10], i, j;
-	for (i = 0; i<10; i++)
-		for (j = 0; j<10; j++)
-
-			a[i][j] = (i + 1)*(j + 1);
-	for (i = 0; i<10; i++) {
-		for (j = 0; j<10; j++)
-			cout << a[i][j] << "\t";
-		cout << endl;
-	}
-}
+	const int arraySize = 10;
+	int s[arraySize];
+	for (int i = 0; i < arraySize; i++)
+		s[i] = 2 + 2 * i;
+	cout << "Element Value" << endl;
+	for (int j = 0; j < arraySize; j++)
+		cout << j << "\t " << s[j] << endl;
 	system("pause");
 
 }
